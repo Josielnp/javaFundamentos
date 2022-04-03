038c9c09a77227e44972751813c7052003de1859
josielnp <josielnp@gmail.com>
2022-04-03 19:14:35 -0300

    Segundo commit. Continuando os estudos

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -1,5 +1,7 @@
 package lambdas;
 
+@FunctionalInterface // interface funcional só pode ter um unico metodo.
+
 public interface Calculo {
 	
 	double executar(double a, double b);
---
+++ exercicios/src/lambdas/CalculoTeste3.java
@@ -0,0 +1,16 @@
+package lambdas;
+
+import java.util.function.BinaryOperator;
+
+public class CalculoTeste3 {
+	
+public static void main(String[] args) {
+		
+		BinaryOperator<Double> calc = (x, y) -> {return x + y;	};
+		System.out.println(calc.apply(2.0, 3.0));
+		
+		calc = (x, y) -> x * y; // outra forma de escrever sem return retorna automatico a multiplicação
+		System.out.println(calc.apply(2.0, 3.0));
+}	
+
+}
